0285c76a2b966ba0289c8f9c63aabf7eb6905e83
Kike <luis17erebsamen@gmail.com>
2020-05-14 19:41:53 -0500

    newCommit

---
+++ Hola.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import styles from './Hola.styles'
+
+
+class Hola extends Component {
+  render() {
+    return (
+      <View>
+        <Text>AHHH  </Text>
+        <Text>ASSS</Text>
+      </View>
+    )
+  }
+}
+
+export default Hola;
+
+
---
+++ Hola.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textoChido: {
+    color: 'purple',
+    borderRadius: '10px'
+  },
+});
+
+export default styles;
