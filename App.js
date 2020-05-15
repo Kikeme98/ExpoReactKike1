c0a15d48a7e6d4f1f299c8411d7f01636c8d57ec
Kike <luis17erebsamen@gmail.com>
2020-05-14 19:41:11 -0500

    newCommit

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hola from './Hola';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Hola></Hola>
+      </View>
+      <View style={styles.body}>
+
+      </View>
+      <View style={styles.footer}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 3,
+    flexDirection: 'column',
   },
+  header: {
+    height: "10%",
+    backgroundColor: 'purple'
+  },
+  body: {
+    height: "80%",
+    backgroundColor: 'cornflowerblue',
+    borderRadius: '20px',
+    margin: '10px',
+    shadowRadius: '10px'
+  },
+  footer:{
+    height: "10%",
+    backgroundColor: 'darkgray' 
+  }
 });
